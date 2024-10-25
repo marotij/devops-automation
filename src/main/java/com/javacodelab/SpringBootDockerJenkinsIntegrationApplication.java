d7a303efb3c947367e4df414453ad66db352b388
maroti Jadhav <tech.maroti92@gmail.com>
2024-10-25 22:19:46 +0530

    first commit

---
+++ src/main/java/com/javacodelab/SpringBootDockerJenkinsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.javacodelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerJenkinsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerJenkinsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javacodelab/SpringBootDockerJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacodelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
